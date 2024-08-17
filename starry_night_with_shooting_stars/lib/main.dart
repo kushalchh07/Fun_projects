0baa0c332407df1248c69fce209f562711ff3ebd
kushalchh07 <kushalchhetri54@gmail.com>
2024-08-17 19:16:39 +0545

    feat: shooting star page added

--- starry_night_with_shooting_stars/lib/main.dart
+++ starry_night_with_shooting_stars/lib/main.dart
@@ -1,125 +1,146 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const StarryNightApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class StarryNightApp extends StatelessWidget {
+  const StarryNightApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Starry Night Proposal',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const StarryNightScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class StarryNightScreen extends StatefulWidget {
+  const StarryNightScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _StarryNightScreenState createState() => _StarryNightScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _StarryNightScreenState extends State<StarryNightScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _shootingStarController;
+  late Animation<Offset> _shootingStarAnimation;
+  late Timer _shootingStarTimer;
+  bool _showMessage = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+
+    _shootingStarController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    _shootingStarAnimation = Tween<Offset>(
+      begin: const Offset(-1.5, -1.5),
+      end: const Offset(1.5, 1.5),
+    ).animate(CurvedAnimation(
+      parent: _shootingStarController,
+      curve: Curves.easeInOut,
+    ));
+
+    _startShootingStarTimer();
+  }
+
+  void _startShootingStarTimer() {
+    _shootingStarTimer = Timer.periodic(const Duration(seconds: 10), (timer) {
+      _shootingStarController.forward(from: 0.0).then((_) {
+        setState(() {
+          _showMessage = true;
+        });
+        Timer(const Duration(seconds: 5), () {
+          setState(() {
+            _showMessage = false;
+          });
+        });
+      });
     });
   }
 
   @override
+  void dispose() {
+    _shootingStarController.dispose();
+    _shootingStarTimer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Stack(
+        children: [
+          _buildStarryBackground(),
+          _buildShootingStar(),
+          _buildMessage(),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    );
+  }
+
+  Widget _buildStarryBackground() {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.black, Colors.blueGrey],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      child: Stack(
+        children: List.generate(100, (index) {
+          return Positioned(
+            left: (index * 20.0) % MediaQuery.of(context).size.width,
+            top: (index * 15.0) % MediaQuery.of(context).size.height,
+            child: Icon(
+              Icons.star,
+              color: Colors.white.withOpacity(0.8),
+              size: 3 + index % 3.0,
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
+  Widget _buildShootingStar() {
+    return SlideTransition(
+      position: _shootingStarAnimation,
+      child: const Icon(
+        Icons.star,
+        color: Colors.yellow,
+        size: 30,
+      ),
+    );
+  }
+
+  Widget _buildMessage() {
+    return Center(
+      child: AnimatedOpacity(
+        opacity: _showMessage ? 1.0 : 0.0,
+        duration: const Duration(seconds: 1),
+        child: const Text(
+          "May Your wish come true",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
